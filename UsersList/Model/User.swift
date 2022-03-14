e1ff5d6d684066118ee0f82a9f22215bc14b683d
tibebit <fabiotibe.bit@gmail.com>
2022-03-14 15:17:35 +0100

    Initial Commit

---
+++ UsersList/Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  UsersList
+//
+//  Created by Fabio Tiberio on 14/03/22.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: UUID
+    let name: String
+    let age: Int
+    let isActive: Bool
+    
+    let company: String
+    let email: String
+    let address: String
+    let about: String
+    let registered: Date
+    let tags: [String]
+    
+    let friends: [UUID: String]
+}
---
+++ UsersList/UsersListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersListApp.swift
+//  UsersList
+//
+//  Created by Fabio Tiberio on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct UsersListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
