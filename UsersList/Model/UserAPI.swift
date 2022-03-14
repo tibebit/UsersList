3236e895547594d8dd9f2c21f4b15f945ebec7da
tibebit <fabiotibe.bit@gmail.com>
2022-03-14 21:20:53 +0100

    add API code to retrieve an array of users

--- UsersList/Model/UserAPI.swift
+++ UsersList/Model/UserAPI.swift
@@ -12,12 +12,12 @@ class UserAPI {
     private var decoder = JSONDecoder()
     private var url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
     
-    func getUsers() -> AnyPublisher<User, Error> {
-        URLSession.shared
+    func getUsers() -> AnyPublisher<[User], Error> {
+        decoder.dateDecodingStrategy = .iso8601
+        return URLSession.shared
             .dataTaskPublisher(for: url)
             .map(\.data)
-            .decode(type: User.self, decoder: decoder)
+            .decode(type: [User].self, decoder: decoder)
             .eraseToAnyPublisher()
     }
-    
 }
