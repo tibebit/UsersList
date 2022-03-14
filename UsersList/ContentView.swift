e5394871157a868352f6fb7b3aecbbd63a53d18e
tibebit <fabiotibe.bit@gmail.com>
2022-03-14 21:22:26 +0100

    Create a list layout to display users

--- UsersList/ContentView.swift
+++ UsersList/ContentView.swift
@@ -6,11 +6,63 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var users = [User]()
+    var api                  = UserAPI()
+    @State private var isShowingError = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 16) {
+                    userCards
+                }
+                .alert("Network Error", isPresented: $isShowingError, actions: {
+                    Button("Ok", role: .cancel) {}
+                })
+                .padding(.vertical, 8)
+            }
+            .navigationTitle("Users List")
+        }
+        .onAppear {
+            let subscription = api.getUsers()
+                .subscribe(on: DispatchQueue.global())
+                .receive(on: DispatchQueue.main)
+                .sink { completion in
+                    if case .failure(_) = completion {
+                        isShowingError = true
+                    }
+                } receiveValue: { users in
+                    self.users = users
+                }
+        }
+    }
+    
+    var userCards: some View {
+        ForEach(users) { user in
+            NavigationLink {
+                Text("\(user.id)")
+            } label: {
+                HStack(alignment: .top) {
+                    Label(user.name, systemImage: "person.circle.fill")
+                        .font(.title3)
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("Age: \(user.age)")
+                        Text(user.isActive ? "Active" : "Inactive")
+                            .foregroundColor(user.isActive ? .green : .red)
+                    }
+                }
+                .foregroundColor(.black)
+                .padding(8)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 14, style: .continuous))
+                .padding(.horizontal, 20)
+                .shadow(radius: 5)
+            }
+        }
     }
 }
 
