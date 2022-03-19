81e6c77ac62bdb26e1d6ddaeba2329b4afd9a692
tibebit <fabiotibe.bit@gmail.com>
2022-03-19 18:59:24 +0100

    Add example of user instance

--- UsersList/Model/Model.swift
+++ UsersList/Model/Model.swift
@@ -21,6 +21,8 @@ struct User: Codable, Identifiable {
     let tags: [String]
     
     let friends: [Friend]
+    
+    static let example = User(id: UUID(), name: "John Lee", age: 20, isActive: true, company: "Microsoft", email: "john.legend@domain.com", address: "555, Awesome Street", about: "I'm passionate about development and fishing. During my free time I like to code APIs to power my side projects", registered: Date.now, tags: ["life-long learner", "engineer", "gamer", ], friends: [Friend(id: UUID(), name: "Lee Yu"), Friend(id: UUID(), name: "Michael Pearson"), Friend(id: UUID(), name: "Timothy Smith")])
 }
 
 struct Friend: Codable, Identifiable {
